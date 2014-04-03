3f54e56b8b75a133249587b1765eac222172f1f6
Mina Asad <mina@mina-asad.com>
2014-04-03 22:31:03 +1100

    Initial Commit

---
+++ SimpleCalculator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SimpleCalculator
+//
+//  Created by Mina Asad on 3/04/2014.
+//  Copyright (c) 2014 mina asad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
